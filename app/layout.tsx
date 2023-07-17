fe0646dbe7f4111530c968afd54c76603825eb2b
João Pedro Bessa <joaopbessa1998@gmail.com>
2023-07-17 11:08:47 +0100

    navbar + footer + constants + types

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer, Navbar } from '@/components'
 import './globals.css'
 import type { Metadata } from 'next'
 
@@ -13,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='relative'>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
