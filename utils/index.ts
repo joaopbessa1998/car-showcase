e5aa3845cf3b826d7bcbfab2a4925b587dd3d1df
João Pedro Bessa <joaopbessa1998@gmail.com>
2023-07-17 18:34:50 +0100

    car angles and generateCarImageUrl

--- utils/index.ts
+++ utils/index.ts
@@ -51,7 +51,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
     const { make, year, model } = car;
 
-    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('customer', 'https://cdn.imagin.studio/getimage');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
     url.searchParams.append('zoomType', 'fullscreen');
