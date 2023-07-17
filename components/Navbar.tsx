4469b7e28382d9666c88a991ca6cc6a426aa3238
João Pedro Bessa <joaopbessa1998@gmail.com>
2023-07-17 18:12:56 +0100

    customButton updated, CarCard, CarDetails, types...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-import { CustomButton } from './CustomButton'
+import CustomButton from "./CustomButton"
 
 const Navbar = () => {
     return (
--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,12 @@
 import Hero from "./Hero"
-import { CustomButton } from "./CustomButton"
 import Navbar from "./Navbar"
+import CustomButton from "./CustomButton"
 import Footer from "./Footer"
 import CustomFilter from "./CustomFilter"
 import SearchBar from "./SearchBar"
 import SearchManufacturer from "./SearchManufacturer"
+import CarCard from "./CarCard"
+import CarDetails from "./CarDetails"
 
 
 export {
@@ -15,4 +17,6 @@ export {
     CustomFilter,
     SearchBar,
     SearchManufacturer,
+    CarCard,
+    CarDetails,
 }
